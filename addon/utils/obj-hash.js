f8027fbd114c0040857d7956139a88c34e047d94
Jacob Jewell <jacobjewell@eflexsystems.com>
2022-09-06 14:39:31 -0400

    cleanup

--- addon/utils/obj-hash.js
+++ addon/utils/obj-hash.js
@@ -1,40 +1,18 @@
 import { tracked } from '@glimmer/tracking';
 
 export default class ObjHash {
-  @tracked contentLength = 0;
   @tracked content = [];
 
-  get length() {
-    return this.contentLength;
-  }
-
   add(obj) {
-    const id = this.generateId();
+    let id = Math.random() * 1000000000000.0;
+    id = parseInt(id);
+    id = '' + id;
+
     this.content[id] = obj;
-    this.contentLength += 1;
     return id;
   }
 
   getObj(key) {
-    const res = this.content[key];
-    if (!res) {
-      throw new Error('no obj for key ' + key);
-    }
-    return res;
-  }
-
-  generateId() {
-    let num = Math.random() * 1000000000000.0;
-    num = parseInt(num);
-    num = '' + num;
-    return num;
-  }
-
-  keys() {
-    const res = [];
-    for (const key in this.content) {
-      res.push(key);
-    }
-    return res;
+    return this.content[key];
   }
 }
