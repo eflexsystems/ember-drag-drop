efaf2c2dbe3c5c11af418146a22d096f84752363
Jacob Jewell <jacobjewell@eflexsystems.com>
2022-09-06 16:30:36 -0400

    update relationships

--- tests/dummy/app/models/book.js
+++ tests/dummy/app/models/book.js
@@ -2,5 +2,5 @@ import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class Book extends Model {
   @attr('string') title;
-  @hasMany('page', { async: true }) pages;
+  @hasMany('page', { inverse: 'book', async: false }) pages;
 }
--- tests/dummy/app/models/page.js
+++ tests/dummy/app/models/page.js
@@ -1,5 +1,6 @@
-import Model, { attr } from '@ember-data/model';
+import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class Page extends Model {
   @attr('string') title;
+  @belongsTo('book', { inverse: 'pages', async: false }) book;
 }
